a233054f145812ff21feee441239bcb97271ab7e
pratibhabinamdar <130791814+pratibhabinamdar@users.noreply.github.com>
2023-04-21 23:44:57 +0530

    2 files modified, 8 added

--- src/app.js
+++ src/app.js
@@ -5,8 +5,38 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+const fs = require('fs');
+
+// Read user data from JSON file
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+
+// Convert JSON data to JavaScript object
+const users = JSON.parse(userData);
+
+// Read account data from JSON file
+const accountData = fs.readFileSync('src/json/accounts.json', {
+  encoding: 'utf8',
+});
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
+});
+
+app.get('/checking', function (req, res) {
+  const checkingData = accounts.checking;
+  res.render('account', { account: checkingData });
+});
+
+app.get('/credit', function (req, res) {
+  const creditAccountData = accounts.credit;
+  res.render('account', { account: creditAccountData });
+});
+// Parse JSON account data into a JavaScript object
+const accounts = JSON.parse(accountData);
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', {
+    title: 'Account Summary',
+    accounts: accounts,
+  });
 });
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
