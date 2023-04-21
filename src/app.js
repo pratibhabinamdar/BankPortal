897ce566cd4ec9dbe2e029fd80ae9293bb31e654
pratibhabinamdar <130791814+pratibhabinamdar@users.noreply.github.com>
2023-04-22 02:08:15 +0530

    4 files modified, 1 added, 1 deleted

--- src/app.js
+++ src/app.js
@@ -6,37 +6,46 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Read user data from JSON file
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const accountData = fs.readFileSync(
+  path.join(__dirname, 'json', 'accounts.json'),
+  'utf8'
+);
+const accounts = JSON.parse(accountData);
 
-// Convert JSON data to JavaScript object
+const userData = fs.readFileSync(
+  path.join(__dirname, 'json', 'users.json'),
+  'utf8'
+);
 const users = JSON.parse(userData);
 
-// Read account data from JSON file
-const accountData = fs.readFileSync('src/json/accounts.json', {
-  encoding: 'utf8',
-});
-app.get('profiles', (req, res) => {
-  res.render('profiles', { user: users[0] });
-});
+app.get('/', (req, res) =>
+  res.render('index', { title: 'Account Summary', accounts: accounts })
+);
 
-app.get('checking', function (req, res) {
-  const checkingData = accounts.checking;
-  res.render('account', { account: checkingData });
+app.get('/savings', (req, res) =>
+  res.render('account', { account: accounts.savings })
+);
+app.get('/checking', (req, res) =>
+  res.render('account', { account: accounts.checking })
+);
+app.get('/credit', (req, res) =>
+  res.render('account', { account: accounts.credit })
+);
+
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
+app.listen(3000, () => {
+  console.log('PS Project Running on port 3000!');
 });
 
 app.get('credit', function (req, res) {
   const creditAccountData = accounts.credit;
   res.render('account', { account: creditAccountData });
 });
-// Parse JSON account data into a JavaScript object
-const accounts = JSON.parse(accountData);
+
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Account Summary',
-    accounts: accounts,
+    accounts,
   });
-});
-app.listen(3000, () => {
-  console.log('PS Project Running on port 3000!');
 });
